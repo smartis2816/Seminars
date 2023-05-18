53348df64a1da12fd9c186d993d80c54848ad249
Gregory <smartis2816@gmail.com>
2023-05-18 20:49:59 +0300

    Initial commit

---
+++ Seminar_1/hw_3.py
@@ -0,0 +1,13 @@
+# Программа загадывает число от 0 до 1000. Необходимо угадать число за 10 попыток.
+# Программа должна подсказывать “больше” или “меньше” после каждой попытки.
+# Для генерации случайного числа используйте код:
+# from random import randint
+# num = randint(LOWER_LIMIT, UPPER_LIMIT)
+
+from random import randint
+
+LOWER_LIMIT = 0
+UPPER_LIMIT = 1000
+
+num = randint(LOWER_LIMIT, UPPER_LIMIT)
+
